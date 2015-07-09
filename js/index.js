76fa5c94c2697a9d124be2fe4e45fbaa2a10c7ea
mparaiso <aikah@free.fr>
2015-07-09 05:43:33 +0200

    pusher test

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ $(function ($) {
     console.log('TRAVIS NOTIFICATIONS - Written and Designed by MParaiso <mparaiso@online.fr>');
     var channels, channel, pusher = new Pusher('5df8ac576dcccf4fd076')
     channel = pusher.subscribe('repo-5372376')
-    pusher.bind_all(function (event) {
-        console.log(event);
+    pusher.bind_all(function (event,data) {
+        console.log(event,arguments);
     });
 
     //var channel = pusher.subscribe('')
