176dbcfdcecf449ea6b9aac2b3734d6ae0bd5957
mparaiso <aikah@free.fr>
2015-07-09 02:03:50 +0200

    js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 /*jslint */
 (function(){
-  console.log('TRAVIS NOTIFICATIONS - Written and Designed by MParaiso <mparaiso@online.fr>");
+  console.log('TRAVIS NOTIFICATIONS - Written and Designed by MParaiso <mparaiso@online.fr>');
 }())
