95b15c8283aae383590590690ff250cf9278eac8
mparaiso <aikah@free.fr>
2015-07-09 03:46:47 +0200

    jquery

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 /*jslint */
-(function(){
-  console.log('TRAVIS NOTIFICATIONS - Written and Designed by MParaiso <mparaiso@online.fr>');
-}())
+$(function ($) {
+    console.log('TRAVIS NOTIFICATIONS - Written and Designed by MParaiso <mparaiso@online.fr>');
+    // get token from travis
+    window.addEventListener("message", function (event) {
+        console.log("received token: " + event.data.token);
+    });
+    window.addEventListener("error", function (event) {
+        console.log("error", event);
+    });
+
+    var iframe = $('<iframe />').hide();
+    iframe.appendTo('body');
+    iframe.attr('src', "https://api.travis-ci.org/auth/post_message");
+})
