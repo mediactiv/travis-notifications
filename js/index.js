79b01c46868d24577b666d93fa53984b8aa4d28f
mparaiso <aikah@free.fr>
2015-07-09 13:46:00 +0200

    index.js

--- js/index.js
+++ js/index.js
@@ -19,10 +19,12 @@ $(function ($) {
             }
         }
     });
+    // creates a notification
     function createBuildNotification(data) {
-        notification = new Notification("Build for \"" + data.repository_slug + "\" finished at "
-            + data.finished_at + " with state: " + data.state, {
+        notification = new Notification("Build for \"" + data.repository_slug , {
             icon: "images/" + (data.state === 'passed' ? 'green_dot.png' : 'red_dot.png'),
+            body: "Build for \"" + data.repository_slug + "\" finished at "
+            + data.finished_at + " with state: " + data.state
         })
         setTimeout(notification.close.bind(notification), 4000);
         return notification
