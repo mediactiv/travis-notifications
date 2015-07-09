e253d3407f00d5176a442a7b9756a33e8125f11d
mparaiso <aikah@free.fr>
2015-07-09 13:44:10 +0200

    notification

--- js/index.js
+++ js/index.js
@@ -3,7 +3,28 @@ $(function ($) {
     console.log('TRAVIS NOTIFICATIONS - Written and Designed by MParaiso <mparaiso@online.fr>');
     var channels, channel, pusher = new Pusher('5df8ac576dcccf4fd076')
     channel = pusher.subscribe('repo-5372376')
-    pusher.bind_all(function (event,data) {
-        console.log(event,arguments);
+    pusher.bind_all(function (event, data) {
+        var notification;
+        console.log(event, arguments);
+        if (event === 'job:finished') {
+            //show notification with data.finished_at,data.repository_slug,data.state
+            if (Notification.permission === "granted") {
+                createBuildNotification(data)
+            } else if (Notification.permission !== "denied") {
+                Notification.requestPermission(function (permission) {
+                    if (permission === 'granted') {
+                        createBuildNotification(data)
+                    }
+                })
+            }
+        }
     });
+    function createBuildNotification(data) {
+        notification = new Notification("Build for \"" + data.repository_slug + "\" finished at "
+            + data.finished_at + " with state: " + data.state, {
+            icon: "images/" + (data.state === 'passed' ? 'green_dot.png' : 'red_dot.png'),
+        })
+        setTimeout(notification.close.bind(notification), 4000);
+        return notification
+    }
 })
